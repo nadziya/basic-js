ba1e70768ab02356e045231e1c1540ee1a5f8dbf
Nadziya Frolova <sunny1976@mail.ru>
2024-12-23 23:40:31 +0400

    feat: complete several tasks

--- src/count-cats.js
+++ src/count-cats.js
@@ -14,9 +14,17 @@ const { NotImplementedError } = require('../extensions/index.js');
  * ]) => 3`
  *
  */
-function countCats(/* matrix */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function countCats(matrix) {
+  let count = 0;
+  let newMatrix = Object.values(matrix);
+  for (let i = 0; i < newMatrix.length; i += 1) {
+      for ( let j = 0; j < newMatrix[i].length; j += 1) {
+         if (newMatrix[i][j] === '^^') {
+          count += 1;
+         }
+      }
+  }
+  return count;
 }
 
 module.exports = {
--- src/delete-digit.js
+++ src/delete-digit.js
@@ -11,9 +11,17 @@ const { NotImplementedError } = require('../extensions/index.js');
  * For n = 152, the output should be 52
  *
  */
-function deleteDigit(/* n */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function deleteDigit(n) {
+  const newN = n.toString();
+  let digitN = [];
+  let result = 0;
+  for (let i = 0; i < newN.length; i += 1) {
+         digitN.push(newN.replace(newN[i],'')); 
+  }
+  if (digitN !== undefined && digitN.length !== 0) {
+      result = Math.max(...digitN);
+  }
+  return result;
 }
 
 module.exports = {
