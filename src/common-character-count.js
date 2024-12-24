3dbaf11a8dbb60f71d74539b12aa970294e0b9d2
Nadziya Frolova <sunny1976@mail.ru>
2024-12-24 13:36:55 +0400

    feat: solve several tasks

--- src/common-character-count.js
+++ src/common-character-count.js
@@ -11,9 +11,24 @@ const { NotImplementedError } = require('../extensions/index.js');
  * For s1 = "aabcc" and s2 = "adcaa", the output should be 3
  * Strings have 3 common characters - 2 "a"s and 1 "c".
  */
-function getCommonCharacterCount(/* s1, s2 */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function getCommonCharacterCount(s1, s2) {
+  const arr1 = [];
+  const arr2 = [];
+  let result = 0;
+  for (let i = 0; i < s1.length; i += 1) {
+     arr1.push(s1[i]);
+  }
+  for (let j = 0; j < s2.length; j += 1) {
+     arr2.push(s2[j]);
+  }
+  for (let k = 0; k < arr2.length; k += 1) {
+      if (arr1.indexOf(arr2[k]) !== -1) {
+          result += 1 ;
+          tempArr1 = arr1.indexOf(arr2[k]);
+          arr1.splice(tempArr1, 1, '');
+      }
+  }
+  return result;
 }
 
 module.exports = {
--- src/dream-team.js
+++ src/dream-team.js
@@ -13,10 +13,31 @@ const { NotImplementedError } = require('../extensions/index.js');
  * createDreamTeam(['Olivia', 1111, 'Lily', 'Oscar', true, null]) => 'LOO'
  *
  */
-function createDreamTeam(/* members */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
-}
+function createDreamTeam(members) {
+  result = [];
+  let arr = [];
+  if (members === undefined ||
+      members === null ||
+      members.length === undefined || 
+      members.length === 0 ||
+      typeof members !== 'object') {
+    result = false;
+  } else {
+     for (let i = 0; i < members.length; i += 1){
+         arr[i]= members[i];
+         console.log(arr[i]);
+      if (typeof arr[i] === 'string') {
+          arr[i] = arr[i].trim();
+          result.push(arr[i][[0]]);
+        }
+    }
+     for (let j = 0; j < result.length; j += 1) {
+          result[j] = result[j].toUpperCase();
+     }
+      result = result.sort().join('');
+  }
+  return result;
+ }
 
 module.exports = {
   createDreamTeam
